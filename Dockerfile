f96e96a9ae0940a19891c418a80eb3064333f835
lev <ivanovleva512@gmail.com>
2024-08-12 23:53:02 +0700

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
 FROM python:3.11.3
 ENV PYTHONUNBUFFERED 1
+RUN git clone https://github.com/ivanovlev02/3.2-crud-docker.git /3.2-crud
+WORKDIR /3.2-crud
+RUN ls .
+RUN pip install -r requirements.txt
+VOLUME /3.2-crud
+EXPOSE 8080
+CMD python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000
--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_front',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'USER': 'netology',
-        'PASSWORD': 'netology12#',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
